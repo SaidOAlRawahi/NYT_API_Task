14652ca43346f071e4e876e1ce06c7eec1df86b7
SaidOAlRawahi <saisalraw211@gmail.com>
2023-04-11 14:17:24 +0400

    done with inserting to sql server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,41 +8,65 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("------------------Welcome to NYT Articles Search------------------");
-        NYTSearchApiResponse articles;
+        NYTSearchApiResponse response;
         while (true){
             System.out.println("What would you want to search about: ");
             String search = sc.next();
             try {
-                articles = getArticlesBySearch(search);
-                for (Article article: articles.response.docs){
-                    System.out.println();
-                    System.out.println(article.headline.main);
-                    System.out.println(article.byline.original);
-                    System.out.println(article.section_name);
-                    System.out.println(article.lead_paragraph);
-                    System.out.println(article.pub_date);
-                    System.out.println("---------------------------------------");
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                response = getArticlesBySearch(search);
+                printArticles(response.response.docs);
+                addArticlesToDB(response.response.docs);
+            } catch (Exception e) {
+                System.out.println(e);
             }
             System.out.println("Do you want to search for more articles (Y/N)?");
             String repeat = sc.next();
             if(!repeat.equalsIgnoreCase("y")){
                 if(!repeat.equalsIgnoreCase("n")){
                     System.out.println("Invalid Input");
-                    break;
                 }
+                break;
             }
         }
     }
+    static void addArticlesToDB(Article[] articles) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=NYT;" + "encrypt=true;"
+                + "trustServerCertificate=true";
 
+        Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+        DriverManager.registerDriver(driver);
+        Connection con = DriverManager.getConnection(url, "sa", "root");
+        for (Article article:articles) {
+            PreparedStatement st = con.prepareStatement("insert into articles\n" +
+                    "values (?,?,?,?,?)");
+            st.setString(1, article.headline.main);
+            st.setString(2, article.byline.original);
+            st.setString(3, article.pub_date);
+            st.setString(4, article.section_name);
+            st.setString(5, article.lead_paragraph);
+            st.executeUpdate();
+        }
+        con.close();
+
+    }
+    static void printArticles(Article[] articles){
+        for (Article article: articles){
+            System.out.println("---------------------------------------");
+            System.out.println(article.headline.main);
+            System.out.println(article.byline.original);
+            System.out.println(article.section_name);
+            System.out.println(article.lead_paragraph);
+            System.out.println(article.pub_date);
+            System.out.println("---------------------------------------");
+        }
+    }
     private static NYTSearchApiResponse getArticlesBySearch(String search) throws IOException {
         String apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+"&api-key=F5sDtSkCBG8qfXGtrJcwktG8YdrIL9pX";
         URL url = new URL(apiUrl);
